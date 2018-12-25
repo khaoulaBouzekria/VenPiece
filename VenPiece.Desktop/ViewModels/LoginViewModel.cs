718e3e3315b71d3672a2027d17983001447110d0
Mohamed Kamel Bouzekria <mohamed.bouzekria@cevital.com>
2018-12-25 18:17:51 +0100

    Adding Mahapps Metro Design the login window

--- VenPiece.Desktop/ViewModels/LoginViewModel.cs
+++ VenPiece.Desktop/ViewModels/LoginViewModel.cs
@@ -1,9 +1,16 @@
-﻿using Caliburn.Micro;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace VenPiece.Desktop.ViewModels
 {
     public class LoginViewModel : Screen
     {
+        VenPieceDbContext db = new VenPieceDbContext();
         
+
     }
 }
--- VenPiece.Desktop/Views/LoginView.xaml.cs
+++ VenPiece.Desktop/Views/LoginView.xaml.cs
@@ -11,13 +11,14 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace VenPiece.Desktop.Views
 {
     /// <summary>
     /// Interaction logic for LoginView.xaml
     /// </summary>
-    public partial class LoginView : Window
+    public partial class LoginView : MetroWindow
     {
         public LoginView()
         {
